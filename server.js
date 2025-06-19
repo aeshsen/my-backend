11c22293f54af738779c36ee46b704bf66561852
fenillahagora <fenil@lahagora.in>
2025-06-19 13:44:40 +0530

    cors_update

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const Video = require('./models/Videoup');
 const Port =  process.env.PORT||5000 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin : "https://video-converter-frontend.netlify.app/",
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+}));
 app.use('/uploads', express.static('uploads'));
 app.use(express.json());
 
